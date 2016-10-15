f2774808b6658f20625f9c84a03db5eac2d03f8e
Zbigniew Osiński <Zbigniew Osiski>
2016-10-16 00:12:47 +0200

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,46 +1,33 @@
-// stałe
-var resolutionA = {x:1440, y:2560};
-var resolutionB = {x:750, y:1334};
+$(function() {
 
-// definicja "klas"
-function Telefon(nazwa, marka, cena, kolor, rozdzielczosc) {
-	console.log('constructor - rozdzielczość: ', rozdzielczosc);
-	this.nazwa = nazwa;
-	this.marka = marka;
-	this.cena = cena;
-	this.kolor = kolor;
-	this.ekran = {
-		x: rozdzielczosc.x,
-		y: rozdzielczosc.y
+	// definicja "klas"
+	function Button(text) {
+		this.text = text || 'Hello';
+	}
+	Button.prototype = {
+		create: function() {
+			var self = this;
+			this.$element = $('<button>');
+			this.$element.text(this.text);
+			this.$element.css('display', 'block');
+			this.$element.click(function() { 
+				alert(self.text); 
+			});
+			$('h1').append(this.$element);
+		}
+	}
+
+	// test
+	test();
+
+	function test() {
+		
+		var testButton = new Button('test button 1');
+		testButton.create();
+
+		var testButton2 = new Button();
+		testButton2.create();	
+
 	};
-};
 
-Telefon.prototype.printInfo = function() {
-	console.log('nazwa: ', this.nazwa, ', marka: ', this.marka, ', cena: ', this.cena, ', kolor: ', this.kolor);
-};
-
-Telefon.prototype.printScreenX = function() {
-	console.log('rozdzielczość ekranu w poziomie: ', this.ekran.x);
-};
-
-Telefon.prototype.printScreenResolution = function() {
-	console.log('rozdzielczosc ekranu: ' + this.ekran.x + ' x ' + this.ekran.y);
-}
-
-// test
-test();
-
-function test() {
-	
-	var iPhone6S = new Telefon('iPhone 6S', 'Apple', 2250, 'biały', resolutionB);
-	var samsungGalaxyS7 = new Telefon('Samsung Galaxy S7', 'Samsung', 2790, 'czarny', resolutionA);
-	resolutionA.x = 1450;
-	var samsungGalaxyS6 = new Telefon('Samsung Galaxy S6', 'Samsung', 1900, 'czarny', resolutionA);
-	
-	samsungGalaxyS7.printInfo();
-	samsungGalaxyS7.printScreenX();
-	samsungGalaxyS7.printScreenResolution();
-
-};
-
-
+});
