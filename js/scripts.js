2451a6fb600a840633ef24b681ed45638f5fbe48
Zbigniew Osiński <Zbigniew Osiski>
2016-10-16 01:27:58 +0200

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,33 @@
-// stałe
-var resolutionA = {x:1440, y:2560};
-var resolutionB = {x:750, y:1334};
-
 // definicja "klas"
-function Telefon(nazwa, marka, cena, kolor, rozdzielczosc) {
-	console.log('constructor - rozdzielczość: ', rozdzielczosc);
-	this.nazwa = nazwa;
-	this.marka = marka;
-	this.cena = cena;
-	this.kolor = kolor;
-	this.ekran = {
-		x: rozdzielczosc.x,
-		y: rozdzielczosc.y
-	};
+function Bicycle(speed) {
+	this.speed = speed;
+};
+function Car(speed) {
+	this.speed = speed;
 };
 
-Telefon.prototype.printInfo = function() {
-	console.log('nazwa: ', this.nazwa, ', marka: ', this.marka, ', cena: ', this.cena, ', kolor: ', this.kolor);
-};
+Bicycle.prototype.calcTravelTime = calcTravelTime;
+Car.prototype.calcTravelTime = calcTravelTime;
 
-Telefon.prototype.printScreenX = function() {
-	console.log('rozdzielczość ekranu w poziomie: ', this.ekran.x);
+function calcTravelTime(distance) {
+	var time = distance / this.speed;
+	return time;
 };
-
-Telefon.prototype.printScreenResolution = function() {
-	console.log('rozdzielczosc ekranu: ' + this.ekran.x + ' x ' + this.ekran.y);
-}
 
 // test
 test();
 
 function test() {
 	
-	var iPhone6S = new Telefon('iPhone 6S', 'Apple', 2250, 'biały', resolutionB);
-	var samsungGalaxyS7 = new Telefon('Samsung Galaxy S7', 'Samsung', 2790, 'czarny', resolutionA);
-	resolutionA.x = 1450;
-	var samsungGalaxyS6 = new Telefon('Samsung Galaxy S6', 'Samsung', 1900, 'czarny', resolutionA);
-	
-	samsungGalaxyS7.printInfo();
-	samsungGalaxyS7.printScreenX();
-	samsungGalaxyS7.printScreenResolution();
+	var myBicicle = new Bicycle(30);
+	console.log('Bicycle');
+	console.log('speed: ' + myBicicle.speed);
+	console.log('travel time: ' + myBicicle.calcTravelTime(10));
+
+	var myCar = new Car(100);
+	console.log('Car');
+	console.log('speed: ' + myCar.speed);
+	console.log('travel time: ' + myCar.calcTravelTime(10));	
 
 };
 
